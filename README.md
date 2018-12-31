a8eaec6c78159096a254478bc6826b70fd3907f5
kenigbolo <expensivestephen@hotmail.com>
2018-12-31 20:36:07 +0200

    Update Readme with example, add mocha for tests, add travis.yml file for CI

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # query-string-data
-A tiny library that helps convert a javascript object into valid query string parameters
+
+A tiny library that helps convert a javascript object into valid query string parameters.
+
+## Example Usage
+
+```javascript
+const {queryString} = require('query-string-object')
+const queryObject = {
+  firstName: 'Stephen',
+  lastName: 'Kenigbolo'
+}
+console.log(queryString(queryObject))
+
+// expected output -> ?firstName=Stephen&lastName=Kenigbolo
+```
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 const {queryString} = require('./index');
 const assert = require('assert');
 
-const obj = {firstName: 'stephen', lastName: 'kenigbolo'};
-
-assert.equal(queryString(obj), '?firstName=stephen&lastName=kenigbolo');
-
-assert.equal(queryString({}), null);
-
-
+describe('Query String Object', () => {
+  describe('#queryString()', () => {
+    it('should save format a given js object to a query string', (done) => {
+      const obj = {firstName: 'stephen', lastName: 'kenigbolo'};
+      assert.equal(queryString(obj), '?firstName=stephen&lastName=kenigbolo');
+      done();
+    });
+    it('should return null for an empty object', (done) => {
+      assert.equal(queryString({}), null);
+      done();
+    });
+  });
+});
